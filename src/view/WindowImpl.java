21626d5dc4cacffc21157dc30ad2d2d4b7c9ebdf
Eugenie <Eugenie@192.168.40.1>
2016-10-20 15:41:37 -0500

    test fonction caca

--- src/view/WindowImpl.java
+++ src/view/WindowImpl.java
@@ -5,6 +5,8 @@ import javax.swing.JFrame;
 public class WindowImpl extends JFrame
 {
 	private static final long serialVersionUID = 2061491136713215502L;
-
 	
+	public void caca(){
+		System.out.println("lolilol");
+	}
 }
