d02857e9d20d78635f8876ca6aaba57157bcc38c
Eugenie <Eugenie@DESKTOP-UG8OUIV>
2016-10-21 12:58:22 -0500

    test 2

--- src/app/MainClass.java
+++ src/app/MainClass.java
@@ -1,9 +1,12 @@
 package app;
 
+import model.SsReaderImpl;
+
 public class MainClass 
 {
 	public static void main(String[] args) 
 	{
-		
+		SsReaderImpl r = new SsReaderImpl();
+		r.lireSs("327146929-v3-29-L1.ss");
 	}
 }
