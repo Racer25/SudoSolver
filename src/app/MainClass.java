c7f35374949aad8d06e0c71e0ecf6a3346ef5540
Charles Cousyn <char-cou@hotmail.fr>
2016-10-22 11:41:03 -0400

    Indentation+réparation caseView+tests dans le main des grilles

--- src/app/MainClass.java
+++ src/app/MainClass.java
@@ -8,6 +8,15 @@ public class MainClass
 	public static void main(String[] args) 
 	{
 		GrilleImpl grilleImpl = new GrilleImpl();
+		grilleImpl.affichage();
 		WindowImpl w = new WindowImpl(grilleImpl);
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		grilleImpl.affichage();
+		
 	}
 }
